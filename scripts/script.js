0814759cbf97f94d73a16d92bc38c6c12ee7d1a7
Vinícius de Sousa Carvalho <Sousav387@gmail.com>
2020-07-30 11:32:30 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -9,7 +9,12 @@ function calculate() {
     var c = Number.parseInt(valuec.value)
     var resultx1 = (- b + Math.sqrt(b**2 - 4 * a * c)) / (2 * a)
     var resultx2 = (- b - Math.sqrt(b**2 - 4 * a * c)) / (2 * a)
-    resx1x2.innerText = `O resultado de x1 é ${resultx1} e de x2 é ${resultx2}`
+    
 
+    if (resultx1 === resultx2) {
+        resx1x2.innerText = `Sua equação possue apenas uma raiz real e seu valor é: ${resultx1}`
+    } else {
+        resx1x2.innerText = `O resultado de x1 é ${resultx1} e de x2 é ${resultx2}`
+    }
     
 }
