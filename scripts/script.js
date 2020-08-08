9e319fa2ab44a36c372395d20123a440ef5e5054
Vinícius de Sousa Carvalho <Sousav387@gmail.com>
2020-08-08 20:03:12 -0300

    update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,3 @@
-/* function calculate2() {
-    var valuea = document.getElementById('valuea')
-    var valueb = document.getElementById('valueb')
-    var valuec = document.getElementById('valuec')
-    var resx1x2 = document.getElementById('resultx1x2')
-   
-    var a = Number.parseInt(valuea.value)
-    var b = Number.parseInt(valueb.value)
-    var c = Number.parseInt(valuec.value)
-    var resultx1 = (- b + Math.sqrt(b**2 - 4 * a * c)) / (2 * a)
-    var resultx2 = (- b - Math.sqrt(b**2 - 4 * a * c)) / (2 * a)
-    
-
-    if (resultx1 === resultx2) {
-        resx1x2.innerText = `Sua equação possue apenas uma raiz real e seu valor é: ${resultx1}`
-    } else {
-        resx1x2.innerText = `O resultado de x1 é ${resultx1} e de x2 é ${resultx2}`
-    }
-    
-} */
-
-
 
 function calculate() {
     var valuea1 = document.getElementById('valuea1')
@@ -35,31 +13,27 @@ function calculate() {
     var valuec1float = Number.parseInt(valuec1.value)
     var valuec2float = Number.parseInt(valuec2.value)
 
-
-    if (valuea2float === NaN) {
-        valuea2float === 1
-    }
-
-    if (valueb2float === NaN) {
-        valueb2float === 1
-    }
-
-    if (valuec2float === NaN) {
-        valuec2float === 1
-    }
-
     var a = valuea1float / valuea2float
     var b = valueb1float / valueb2float
     var c = valuec1float / valuec2float
 
+    var resultx1numerator = (- b + Math.sqrt(b**2 - 4 * a * c))
+    var resultx1denominator = 2 * a
+
+    var resultx2numerator = (- b - Math.sqrt(b**2 - 4 * a * c))
+    var resultx2denominator = 2 * a
 
     var resultx1 = (- b + Math.sqrt(b**2 - 4 * a * c)) / (2 * a)
+
     var resultx2 = (- b - Math.sqrt(b**2 - 4 * a * c)) / (2 * a)
 
+
     if (resultx1 === resultx2) {
-        results.innerText = `Sua equação possue apenas uma raiz real e seu valor é: ${resultx1}`
+        results.innerText = `Sua equação possue apenas uma raiz real e seu valor é em fração ${resultx1numerator}/${resultx1denominator} e também pode ser apresentado dessa forma: ${resultx1}`
     } else {
-        results.innerText = `O resultado de x1 é ${resultx1} e de x2 é ${resultx2}`
+        results.innerText = `O resultado de x1 é ${resultx1numerator}/${resultx1denominator} ou ${resultx1} e de x2 é ${resultx2numerator}/${resultx2denominator} ou ${resultx2}`
     }
+
+    console.log()
 
 }
